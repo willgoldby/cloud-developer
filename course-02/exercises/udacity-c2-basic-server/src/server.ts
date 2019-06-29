edb795959ad110c395f68912b8ae4cc28ca2a0f5
Jenna Lawler <jennalawler@jennas-imac.lan>
2019-06-29 16:37:02 -0400

    my end points added

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -71,9 +71,22 @@ import { Car, cars as cars_list } from './cars';
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
 
+    app.get("/cars/", (req: Request, res: Response) =>
+    {
+      let { make } = req.query;
+      let cars_list = cars;
+
+      if (make){
+        cars_list = cars_list.filter((veh) => veh.make === make);
+      }
+      return res.status(200).send(cars_list);
+    });
+  
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+
+ 
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
