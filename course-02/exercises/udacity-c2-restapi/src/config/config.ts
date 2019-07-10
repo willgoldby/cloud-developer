e5314465e560189c7f4a1f111d11e9e941a895aa
Jenna Lawler <jennalawler@jennas-imac.lan>
2019-07-09 20:28:52 -0400

    getting 502 Bad Gateway

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -5,7 +5,7 @@ export const config = {
     "database": process.env.database,
     "host": process.env.host,
     "dialect": process.env.dialect,
-    "aws_reigion": process.env.aws_reigion,
+    "aws_region": process.env.aws_reigion,
     "aws_profile": process.env.aws_profile,
     "aws_media_bucket": process.env.aws_media_bucket
   },
