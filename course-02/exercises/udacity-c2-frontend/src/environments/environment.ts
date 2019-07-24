77da4087eeb4d87721002ecda02e0085e74554eb
Jenna Lawler <jennalawler@jennas-imac.lan>
2019-07-23 20:21:56 -0400

    push

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'udagram-goldby-dev-dev.us-east-2.elasticbeanstalk.com'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
